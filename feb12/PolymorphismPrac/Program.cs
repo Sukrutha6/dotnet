b300b8901758e71445e17c0a1691bc21031d357a
Pochammala Sukrutha <118876735+Sukrutha6@users.noreply.github.com>
2026-02-12 16:22:56 +0530

    Add files via upload

---
+++ feb12/PolymorphismPrac/Program.cs
@@ -0,0 +1,69 @@
+﻿// 1. Create a base class `Shape` with a method `Draw()`. Derive `Circle` and `Square` classes that override the `Draw()` method.
+
+// using System;
+// class Shape
+// {
+//     public virtual void Draw()
+//     {
+//         Console.WriteLine("Drawing a shape");
+//     }
+// }
+// class Circle : Shape
+// {
+//     public override void Draw()
+//     {
+//         Console.WriteLine("Drawing a circle");
+//     }
+// }
+// class Square : Shape
+// {
+//     public override void Draw()
+//     {
+//         Console.WriteLine("Drawing a square");
+//     }
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         Shape s1 = new Circle();
+//         s1.Draw();
+//         Shape s2 = new Square();
+//         s2.Draw();
+//     }
+// }
+
+// 2. Implement a `Payment` class with a method `ProcessPayment()`. Extend it to `CreditCardPayment` and `PayPalPayment` with different implementations.
+
+using System;
+class Payment
+{
+    public virtual void ProcessPayment()
+    {
+        Console.WriteLine("Processing payment");
+    }
+}
+class CreditCradPayment : Payment
+{
+    public override void ProcessPayment()
+    {
+        Console.WriteLine("Processing credit card payment");
+    }
+}
+class PayPalPayment : Payment
+{
+    public override void ProcessPayment()
+    {
+        Console.WriteLine("Processing PayPal payment");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Payment p1 = new CreditCradPayment();
+        p1.ProcessPayment();
+        Payment p2 = new PayPalPayment();
+        p2.ProcessPayment();
+    }
+}
---
+++ feb12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static bool Valid(string email)
+    {
+        bool check = false;
+
+        if (email.EndsWith("@gmail.com") &&
+            email.Count(c => c == '@') == 1 &&
+            email.Count(c => c == '.') == 1)
+        {
+            check = true;
+        }
+
+        return check;
+    }
+
+    public static void Main()
+    {
+        string email = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(email))
+        {
+            Console.WriteLine(false);
+            return;
+        }
+
+        bool check = Valid(email);
+        Console.WriteLine(check);
+    }
+}
